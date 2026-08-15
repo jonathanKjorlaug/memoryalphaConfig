613d4cbedd91de8cbc44073a321ddf0b5841544b
Jonathan Kjørlaug <jonathan@kjorlaug.no>
2026-08-15 15:02:17 +0200

    Added git

--- configuration.nix
+++ configuration.nix
@@ -43,6 +43,7 @@
     environment.systemPackages = with pkgs; [
         neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         wget
+        git
     ];
 
     # Some programs need SUID wrappers, can be configured further or are
