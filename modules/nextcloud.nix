a934e684b6cb985a040de8fe4747cbbc85087b07
Jonathan Kjørlaug <jonathan@kjorlaug.no>
2026-08-15 15:22:46 +0200

    Starting to experiment with nextcloud

---
+++ modules/nextcloud.nix
@@ -0,0 +1,17 @@
+{...}: {
+    services.nextcloud = {
+        enable = true;
+        hostName = "localhost";
+        config.adminpassFile = "/etc/nextcloud-admin-ass";
+        config.dbtype = "sqlite";
+        settings = {
+            settings = {
+                # Some sane defaults required to satisfy Nextcloud configuration check
+                maintenance_window_start = 1;
+                default_phone_region = "DE";
+                log_type = "systemd";
+                serverid = 0;
+            };
+        };
+    };
+}
